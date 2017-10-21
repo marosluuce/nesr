7b666030a275aa4399726a8b9a50921ec1488aa6
Nathan Walker <earthworn@gmail.com>
2017-10-20 20:40:26 -0500

    Add CLC, NOP, TSX, TXA, TXS, and TYA.

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,13 +4,13 @@ use instruction::Instruction;
 #[derive(PartialEq, Debug)]
 pub struct Cpu {
     pub cycles: u64,
-    program_counter: u16,
-    stack_pointer: u8,
+    pub program_counter: u16,
+    pub stack_pointer: u8,
     pub accumulator: u8,
     pub x: u8,
     pub y: u8,
     pub status: u8,
-    ram: Ram,
+    pub ram: Ram,
 }
 
 impl Cpu {
@@ -22,42 +22,79 @@ impl Cpu {
             accumulator: 0,
             x: 0,
             y: 0,
-            status: 0,
+            status: 0b00100000,
             ram: Ram::new(),
         }
     }
 
     pub fn execute(&mut self, instruction: Instruction) {
         match instruction {
+            Instruction::CLC => {
+                self.cycles += 2;
+                self.status &= !0b00000001;
+            }
             Instruction::DEX => {
                 self.x = self.x.wrapping_sub(1);
                 self.cycles += 2;
-                self.status = calculate_status(self.x)
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.x)
             }
             Instruction::DEY => {
                 self.y = self.y.wrapping_sub(1);
                 self.cycles += 2;
-                self.status = calculate_status(self.y)
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.y)
             }
             Instruction::INX => {
                 self.x = self.x.wrapping_add(1);
                 self.cycles += 2;
-                self.status = calculate_status(self.x)
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.x)
             }
             Instruction::INY => {
                 self.y = self.y.wrapping_add(1);
                 self.cycles += 2;
-                self.status = calculate_status(self.y)
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.y)
+            }
+            Instruction::NOP => {
+                self.cycles += 2;
             }
             Instruction::TAX => {
                 self.x = self.accumulator;
                 self.cycles += 2;
-                self.status = calculate_status(self.x)
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.x)
             }
             Instruction::TAY => {
                 self.y = self.accumulator;
                 self.cycles += 2;
-                self.status = calculate_status(self.y)
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.y)
+            }
+            Instruction::TSX => {
+                self.x = self.stack_pointer;
+                self.cycles += 2;
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.x)
+            }
+            Instruction::TXA => {
+                self.accumulator = self.x;
+                self.cycles += 2;
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.accumulator)
+            }
+            Instruction::TXS => {
+                self.stack_pointer = self.x;
+                self.cycles += 2;
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.stack_pointer)
+            }
+            Instruction::TYA => {
+                self.accumulator = self.y;
+                self.cycles += 2;
+                self.status &= !0b10000010;
+                self.status |= calculate_status(self.accumulator)
             }
             _ => {}
         }
@@ -69,9 +106,9 @@ fn calculate_status(new: u8) -> u8 {
 }
 
 fn negative(new: u8) -> u8 {
-    new & 0b100000
+    new & 0b10000000
 }
 
 fn zero(new: u8) -> u8 {
-    return if new == 0 { 0b00010 } else { 0 };
+    return if new == 0 { 0b00000010 } else { 0b00000000 };
 }
--- src/instruction.rs
+++ src/instruction.rs
@@ -1,21 +1,33 @@
 pub enum Instruction {
+    CLC = 0x18,
     DEX = 0xCA,
     DEY = 0x88,
     INX = 0xE8,
     INY = 0xC8,
+    NOP = 0xEA,
     TAX = 0xAA,
     TAY = 0xA8,
+    TSX = 0xBA,
+    TXA = 0x8A,
+    TXS = 0x9A,
+    TYA = 0x98,
     UNKNOWN = 0x00,
 }
 
 pub fn to_instruction(string: &'static str) -> Instruction {
     match string {
+        "clc" => Instruction::CLC,
         "dex" => Instruction::DEX,
         "dey" => Instruction::DEY,
         "inx" => Instruction::INX,
         "iny" => Instruction::INY,
+        "nop" => Instruction::NOP,
         "tax" => Instruction::TAX,
         "tay" => Instruction::TAY,
+        "tsx" => Instruction::TSX,
+        "txa" => Instruction::TXA,
+        "txs" => Instruction::TXS,
+        "tya" => Instruction::TYA,
         _ => Instruction::UNKNOWN,
     }
 }
